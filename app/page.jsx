d3c938d3504585a7bba8194be983897001788bf9
Vinith69 <ramarock09@gmail.com>
2025-04-12 08:33:13 +0530

    done test

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+"use client";
+import { CldUploadWidget } from "next-cloudinary";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <CldUploadWidget
+          uploadPreset="test-preset"
+          options={{
+            sources: ["local"],
+            maxFileSize: 1000000,
+            multiple: false,
+            clientAllowedFormats: ["image"],
+          }}
+        >
+          {({ open }) => {
+            return <button onClick={() => open()}>Upload an Image</button>;
+          }}
+        </CldUploadWidget>
+      </main>
+    </div>
+  );
+}
